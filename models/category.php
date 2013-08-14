50f19284a97e4f0656997e800f8d4a0ebc20161f
blzkz.blzkz <sergiobl88@gmail.com>
2013-08-14 10:26:50 +0200

    adding get method to category model

--- models/category.php
+++ models/category.php
@@ -1,5 +1,10 @@
 <?php
 class category extends model
 {
-	
+	public function get()
+	{
+		$query = self::$con->query("SELECT * FROM categories ORDER BY id DESC");
+		return $query->fetchAll();
+	}
+
 }
