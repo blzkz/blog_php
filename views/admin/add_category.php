a1a771337fb586804d43397bae45b3b26a60700c
blzkz.blzkz <sergiobl88@gmail.com>
2013-09-18 22:55:16 +0200

    Adding required stament to some inputs

--- views/admin/add_category.php
+++ views/admin/add_category.php
@@ -2,7 +2,7 @@
 		<form action="<?php echo base_url().'admin/controlCategory';?>" method="post">
 			<div class='container_12'>
 				<div class='grid_2' style='text-align:right'><label for='name'>Name: </label></div>
-				<div class='grid_10'><input name="name" id='name' type="text"></div>
+				<div class='grid_10'><input name="name" id='name' type="text" required></div>
 			</div>
 			<input type="submit" name="submit" value="Enviar">
 		</form>
--- views/admin/article.php
+++ views/admin/article.php
@@ -2,15 +2,15 @@
 		<form action="<?php echo base_url().'admin/control';?>" method="post">
 			<div class='container_12'>
 				<div class='grid_2' style='text-align:right'><label for='title'>Title: </label></div>
-				<div class='grid_10'><input name="title" id='title' type="text"></div>
+				<div class='grid_10'><input name="title" id='title' type="text" required></div>
 			</div>
 			<div class='container_12'>
 				<div class='grid_2' style='text-align:right'><label for='url'>Image url: </label></div>
-				<div class='grid_10'><input name="url" id='url' type="text"></div>
+				<div class='grid_10'><input name="url" id='url' type="text" required></div>
 			</div>
 			<div class='container_12'>
 				<div class='grid_2' style='text-align:right'><label for='content'>Content: </label></div>
-				<div class='grid_10'><textarea name="content" id="content"></textarea></div>
+				<div class='grid_10'><textarea name="content" id="content" required></textarea></div>
 			</div>
 			<input type="submit" name="submit" value="Enviar">
 		</form>
