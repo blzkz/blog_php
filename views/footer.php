d00aa4673216bcdf44a5bcb09543d9ec8c854a73
Sergio Blazquez <sergiobl88@gmail.com>
2016-03-08 14:06:09 +0000

    adapting the homepage to responsive design

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,5 @@
 		<div class="push"></div>
 		</div>
-		<footer>
 		<script type="text/javascript">
 	    $(window).load(function() {
 	        $('#slider').nivoSlider({
@@ -8,24 +7,6 @@
 			directionNav: 'false'});
 	    });
 	    </script>
-	    <div class="container">
-	    	<div class='footer'>Diseño y programación por blzkz. Todos los derechos reservados.<br>
-	    	<?php
-				if (isset($_SESSION['nick'])) {?>
-					<?php echo $_SESSION['nick'];?> <a href="<?php echo base_url().'login/logout';?>">Logout</a>
-					<?php
-					if ($_SESSION['nick'] === 'blzkz')
-					{?>
-					<a href="<?php echo base_url().'admin';?>">Admin</a>
-				<?php } 
-				}
-				else 
-				{ ?>
-					<a href="<?php echo base_url().'login';?>">Login</a>
-				<?php } ?>	
-	    	</div>
-	    </div>
-	    </footer>
 	    <nav class="navbar navbar-inverse navbar-fixed-bot">
 		    <div class="container">
 		    <div class='footer'>
