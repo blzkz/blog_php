acf3f173e893732442f56e905f8427476c37bfb6
Sergio Blazquez <sergiobl88@gmail.com>
2016-03-26 21:59:49 +0000

    Login using bootstrap

--- views/login.php
+++ views/login.php
@@ -1,18 +1,19 @@
-
-		<h1>Login</h1>
-		<div class='container_6'>
+			<div class='row'>
+				<div class='col-md-12'>
+					<h1>Login</h1>
+				</div>
+			</div>
 			<?php if ($error) {?>
 			<h4>Nick or password incorrect</h4>
 			<?php }?>
 			<form action="<?php echo base_url().'login/control';?>" method="post">
-				<div class='container_12'>
-					<div class='grid_2' style='text-align:right'><label for='nick'>Nick: </label></div>
-					<div class='grid_2'><input name="nick" id='nick' type="text"></div>
+				<div class='row'>
+					<div class='col-md-1'><label for='nick'>Nick: </label></div>
+					<div class='col-md-2'><input name="nick" id='nick' type="text"></div>
 				</div>
-				<div class='container_12'>
-					<div class='grid_2' style='text-align:right'><label for='password'>Password: </label></div>
-					<div class='grid_2'><input name="password" id='password' type="password"></div>
+				<div class='row'>
+					<div class='col-md-1'><label for='password'>Password: </label></div>
+					<div class='col-md-2'><input name="password" id='password' type="password"></div>
 				</div>
-			<div class='prefix_1'><input type="submit" name="submit" value="Enviar"></div>
+			<div class='col-md-2 col-md-offset-1'><input type="submit" name="submit" value="Enviar"></div>
 			</form>
-		</div>
